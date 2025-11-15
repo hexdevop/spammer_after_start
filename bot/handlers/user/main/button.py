41047a3712ece473fcc15ed3e032f57aa81fb5da
hexdevop <abduraxmonislomov@gmail.com>
2025-11-16 02:27:41 +0500

    Spam stop by button prohibition

--- bot/handlers/user/main/button.py
+++ bot/handlers/user/main/button.py
@@ -1,7 +1,7 @@
 from aiogram import types, Router
 
 from bot.filters.buttons import Button
-from bot.services import PostManager
+# from bot.services import PostManager
 from bot.utils import helper
 from config import config
 
@@ -11,8 +11,8 @@ router = Router()
 @router.message(Button('button'))
 async def button(
         message: types.Message,
-        post_manager: PostManager,
+        # post_manager: PostManager,
 ):
-    post_manager.stop_user_spam(message.from_user.id)
+    # post_manager.stop_user_spam(message.from_user.id)
     config.counter += 1
     await helper.send_hi_views(message.from_user.id, message.message_id, message.from_user.first_name, message.from_user.language_code, startplace=False)
